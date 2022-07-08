fca378343cbd9ee9bf0eefdca45fbd33850dfd57
Kyrychuk anton <6377200@stud.nau.edu.ua>
2022-07-08 14:37:06 +0300

    s

--- service/windows/service.cpp
+++ service/windows/service.cpp
@@ -1,73 +1,76 @@
+#include "service.h"
 #include <boost/function.hpp>
-#include <boost/bind.hpp>
-#include <functional>
-#define init();
-#define debug();
-#include "debug.h"
-
-#include <string.h>
-#include <windows.h>
+#include "synchapi.h"
 #include <exception>
 #include <stdexcept>
-#include <string>
-
 #include "winnt.h"
 #include "winsvc.h"
 
-class ServiceWrapper{
-public:
-  static void start(const std::string &_name){
-    debug("Constructor\n");
+SERVICE_STATUS ServiceWrapper::serviceStatus = {0};
+SERVICE_STATUS_HANDLE ServiceWrapper::serviceStatusHandle = NULL;
+HANDLE ServiceWrapper::stopEvent = 0;
+SERVICE_TABLE_ENTRY ServiceWrapper::serviceTable[] = {0};
+std::string ServiceWrapper::serviceName;
+std::function<void(std::string, bool)> ServiceWrapper::startFunc;
+
+void ServiceWrapper::start(const std::string &_name) {
+
     serviceName = _name;
     serviceTable[0].lpServiceName = (LPSTR)_name.c_str();
     serviceTable[0].lpServiceProc = (LPSERVICE_MAIN_FUNCTION)serviceMain;
-    if (StartServiceCtrlDispatcher (serviceTable) == false){
+    if (StartServiceCtrlDispatcher(serviceTable) == false) {
       throw(std::runtime_error("StartServiceCtrlDispatcher failed"));
     }
-    serviceStatusHandle = RegisterServiceCtrlHandler(serviceName.c_str(),serviceControlHandler);
-    if(!serviceStatusHandle)throw(std::runtime_error("RegisterServiceCtrlHandler failed"));
+  }
+
+void ServiceWrapper::serviceMain(DWORD argc, LPTSTR *argv) {
+    serviceStatusHandle =
+        RegisterServiceCtrlHandler(serviceName.c_str(), serviceControlHandler);
+    if (!serviceStatusHandle)
+      throw(std::runtime_error("RegisterServiceCtrlHandler failed"));
     serviceStatus.dwServiceType = SERVICE_WIN32_OWN_PROCESS;
+    serviceStatus.dwCurrentState = SERVICE_START_PENDING;
+    if (!SetServiceStatus(serviceStatusHandle, &serviceStatus))
+      throw(std::runtime_error("SetServiceStatus failed"));
+
+    stopEvent = CreateEvent(NULL, TRUE, FALSE, NULL);
+    if (!stopEvent) {
+      serviceStatus.dwCheckPoint = 1;
+      serviceStatus.dwCurrentState = SERVICE_STOPPED;
+      serviceStatus.dwWin32ExitCode = GetLastError();
+      if (!SetServiceStatus(serviceStatusHandle, &serviceStatus))
+        throw(std::runtime_error("SetServiceStatus failed"));
+      return;
+    }
+
+    startFunc(serviceName, true);
+
+    serviceStatus.dwControlsAccepted = SERVICE_ACCEPT_STOP;
     serviceStatus.dwCurrentState = SERVICE_RUNNING;
-    if(!SetServiceStatus(serviceStatusHandle, &serviceStatus))throw(std::runtime_error("SetServiceStatus failed"));
+    if (!SetServiceStatus(serviceStatusHandle, &serviceStatus))
+      throw(std::runtime_error("SetServiceStatus failed"));
 
+    while (true) {
+      WaitForSingleObject(stopEvent, INFINITE);
+      startFunc(serviceName, false);
+      serviceStatus.dwCurrentState = SERVICE_STOPPED;
+      if (!SetServiceStatus(serviceStatusHandle, &serviceStatus))
+        throw(std::runtime_error("SetServiceStatus failed"));
+      return;
+    }
   }
-private:
-  static SERVICE_STATUS serviceStatus;
-  static SERVICE_STATUS_HANDLE serviceStatusHandle;
-  static HANDLE event;
-  static SERVICE_TABLE_ENTRY serviceTable[2];
-  static std::string serviceName;
 
-  static void serviceMain (DWORD argc, LPTSTR *argv){
-    debug("ServiceMain()\n");
-    debug(serviceName.c_str());
-  }
-  static void serviceControlHandler (DWORD CtrlCode){
-    debug("serviceControlHandler\n");
+void ServiceWrapper::serviceControlHandler(DWORD CtrlCode) {
+
     switch (CtrlCode) {
-      case SERVICE_CONTROL_STOP :
+      case SERVICE_CONTROL_STOP:
+        serviceStatus.dwCurrentState = SERVICE_STOP_PENDING;
+        if (!SetServiceStatus(serviceStatusHandle, &serviceStatus))
+          throw(std::runtime_error("SetServiceStatus failed"));
+        SetEvent(stopEvent);
         break;
       default:
         break;
     }
   }
-};
 
-
-  SERVICE_STATUS ServiceWrapper::serviceStatus = {0};
-  SERVICE_STATUS_HANDLE ServiceWrapper::serviceStatusHandle = NULL;
-  HANDLE ServiceWrapper::event = INVALID_HANDLE_VALUE;
-  SERVICE_TABLE_ENTRY ServiceWrapper::serviceTable[] = {0};
-  std::string ServiceWrapper::serviceName;
-
-
-int main(int argc, TCHAR *argv[]) {
-  initdebug();
-
-  debug("main() : startup\n");
-
-  ServiceWrapper::start("Socks5");
-
-  closesocket(debugsock);
-  return 0;
-}
