b17fc61532ec1d72bd43c0b870230a4d9c3a6f9d
Kyrychuk anton <6377200@stud.nau.edu.ua>
2022-07-12 00:18:42 +0300

    service working

--- service/windows/debug.h
+++ service/windows/debug.h
@@ -1,16 +1,29 @@
 #include <winsock2.h>
 #include <stdio.h>
 
+#ifdef DEBUG_AUX
+inline unsigned int getIPAddress(char a, char b, char c, char d);
+extern const int IP;
+extern const int PORT;
+extern const char SOCKOPTION;
+extern struct sockaddr_in servaddr;
+extern SOCKET debugsock;
+#elif defined(DEBUG)
 inline unsigned int getIPAddress(char a, char b, char c, char d) {
   return (((a & 0xff) << 24) | ((b & 0xff) << 16) | ((c & 0xff) << 8) |
           (d & 0xff));
 }
-
 const int IP = getIPAddress(127, 0, 0, 1);
 const int PORT = 8080;
 const char SOCKOPTION = 1;
 struct sockaddr_in servaddr;
 SOCKET debugsock;
+#endif
+
+
+
+#if defined (DEBUG_AUX) || defined (DEBUG)
+#define debug(x){send(debugsock, (x), strlen(x), 0);}
 
 #define initdebug(){\
   WSADATA wsaData = {0};\
@@ -25,6 +38,7 @@ SOCKET debugsock;
     return -1;\
   }\
 }
-
-#define debug(x){send(debugsock, (x), strlen(x), 0);}
-
+#else
+  #define debug(x){}
+  #define initdebug(){}
+#endif
