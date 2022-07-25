d15ee8942b474031ebab1ebc460f294bf4d9c8f6
Oleh Klepatskyi <o.klepatskyi@gmail.com>
2022-07-25 12:25:50 +0300

    Server fields update

--- src/server.cpp
+++ src/server.cpp
@@ -2,10 +2,10 @@
 #include <thread>
 
 TcpServer::TcpServer(ba::io_context& io_context, unsigned short port, size_t bufferSizeKB,
-	int maxSessions): 
-	io_context_(io_context),
-	acceptor_(io_context, ba::ip::tcp::endpoint(ba::ip::tcp::v4(), port)),
-	bufferSizeKB_(bufferSizeKB),
+	size_t maxSessions): 
+	_io_context(io_context),
+	_acceptor(io_context, ba::ip::tcp::endpoint(ba::ip::tcp::v4(), port)),
+	_bufferSizeKB(bufferSizeKB),
 	_pool(std::thread::hardware_concurrency() * 2),
 	_sessions(0),
 	_maxSessions(maxSessions)
@@ -26,10 +26,10 @@ void TcpServer::start_accept()
 	}
 	
 	log(TRACE_LOG) << "[server] Current connections: " << _sessions;
-	log(TRACE_LOG) << "[server] waiting for new client... " << acceptor_.local_endpoint();
+	log(TRACE_LOG) << "[server] waiting for new client... " << _acceptor.local_endpoint();
 	
-	TcpSession::pointer new_connection = TcpSession::create(this, io_context_, bufferSizeKB_);
-	acceptor_.async_accept(new_connection->socket(),
+	TcpSession::pointer new_connection = TcpSession::create(this, _io_context, _bufferSizeKB);
+	_acceptor.async_accept(new_connection->socket(),
 		boost::bind(&TcpServer::handle_accept, this, new_connection,
 			boost::asio::placeholders::error));
 }
--- src/server.h
+++ src/server.h
@@ -8,19 +8,19 @@
 class TcpServer
 {
 public:
-	TcpServer(ba::io_context& io_context, unsigned short port, size_t bufferSizeKB, int maxSessions);
+	TcpServer(ba::io_context& io_context, unsigned short port, size_t bufferSizeKB, size_t maxSessions);
 private:
 	void start_accept();
 	void handle_accept(TcpSession::pointer new_connection, const boost::system::error_code& error);
 
 	friend class TcpSession;
 
-	ba::io_context& io_context_;
-	ba::ip::tcp::acceptor acceptor_;
+	ba::io_context& _io_context;
+	ba::ip::tcp::acceptor _acceptor;
 	ba::thread_pool _pool;
-	const size_t bufferSizeKB_;
+	const size_t _bufferSizeKB;
 	std::atomic_int _sessions;
-	const int _maxSessions;
+	const size_t _maxSessions;
 };
 
 #endif // _SERVER_H_
