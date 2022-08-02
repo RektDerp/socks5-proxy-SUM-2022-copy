326163c7c1d89afa8543866754b42bf6b7045deb
Segimara <76587473+Segimara@users.noreply.github.com>
2022-08-02 18:52:49 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -24,7 +24,7 @@
 	wchar_t pwd[PATHSIZE];
 	char utf8_pwd[PATHSIZE];
 
-	void INIT()
+	void init()
 	{
 		SetConsoleCP(1251); 
 		SetConsoleOutputCP(1251); 
@@ -33,7 +33,7 @@
 		WideCharToMultiByte(CP_UTF8, 0, pwd, -1, utf8_pwd, PATHSIZE, NULL, NULL); 
 		strcpy(defaultDatabasePath, utf8_pwd); 
 		strcat(defaultDatabasePath, "sessions_stat.db"); 
-		
+	
 		GetModuleFileNameA(NULL, utf8_pwd, PATHSIZE); 
 		* (strrchr(utf8_pwd, '\\') + 1) = '\0'; 
 		strcpy(defaultConfigPath, utf8_pwd); 
@@ -42,24 +42,19 @@
 		strcat(defaultLogPath, "server.log");
 	}
 #else
-	void INIT()
-	{
-
-	}
 	char defaultLogPath[PATHSIZE] = "/tmp/server.log";
 	const char defaultConfigPath [PATHSIZE] = "/etc/socks5-config.txt";
 	const char defaultDatabasePath[PATHSIZE] = "/tmp/sessions_stat.db";
+
+	void init(){}
 #endif
 
 int main(int argc, char** argv)
 {
 	using namespace proxy;
-	INIT();
+	init();
 	Logger::logFileName = defaultLogPath;
 	ConfigReader::configFilePath = defaultConfigPath;
-	std::cout << "Config path: " << ConfigReader::configFilePath << std::endl;
-	std::cout << "Log path: " << Logger::logFileName << std::endl;
-	std::cout << "Database path: " << defaultDatabasePath << std::endl;
 	ConfigReader& config = ConfigReader::getInstance();
 	config.dumpFileValues();
 	try {
